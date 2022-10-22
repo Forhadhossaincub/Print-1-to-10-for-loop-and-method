6d5bedf2332c1f633a57e42abcff2ea27fa987da
NET <NET@DESKTOP-NSMUMFG>
2022-10-22 11:20:25 +0600

    Add project files.

---
+++ Print 1 to 10 for loop and method/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_1_to_10_for_loop_and_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Print();
+
+
+            Console.ReadKey();
+        }
+
+        public static void Print()
+        {
+            for (int i = 1; i <=10; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+          
+        }
+    }
+}
